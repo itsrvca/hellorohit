96b4ee5d69b79f225cb606668bb39e2945da44ce
Tarun Singh <tarunrajput639@gmail.com>
2021-01-20 17:26:47 +0530

    Second commit

--- app/src/main/java/com/androcoder/hellorohit/MainActivity.java
+++ app/src/main/java/com/androcoder/hellorohit/MainActivity.java
@@ -3,12 +3,26 @@ package com.androcoder.hellorohit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button button;
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        textView = findViewById(R.id.textview);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText("Hello world rohit this side");
+            }
+        });
     }
 }
